b2ce9488603bb434bc70806556e0edfce9d725a1
Xuân Tin <xuantinfx@gmail.com>
2019-04-01 13:28:45 +0700

    refactor utils

--- flaskr/utils/__init__.py
+++ flaskr/utils/__init__.py
@@ -2,18 +2,16 @@ import os
 from config import Config as config
 from flaskr.models.random_forest import RandomForest
 from flaskr.models.gradient_boosting import GradientBoosting
+from flaskr.models.lstm import Lstm
 
+available_models = {
+    "random_forest": RandomForest, 
+    "gradient_boosting": GradientBoosting, 
+    "lstm": Lstm
+}
 
 def get_available_model_names():
-    dirs = os.listdir(config.MODEL_DIR)
-    result = []
-    for file in dirs:
-        if(file.find('.py') != -1 and file != 'base_model.py'):
-            result.append(file[:-3])  # exclude '.py'
-        else:
-            continue
-    return result
-
+    return list(available_models.keys())
 
 def get_available_exported_model_names():
     dirs = os.listdir(config.EXPORTED_MODELS_DIR)
@@ -24,9 +22,7 @@ def get_available_exported_model_names():
 
 
 def ModelFactory(market_info, model_name,  candle_size, train_daterange):
-    if(model_name == 'random_forest'):
-        return RandomForest(market_info, model_name, candle_size, train_daterange)
-    elif(model_name == 'gradient_boosting'):
-        return GradientBoosting(market_info, model_name, candle_size, train_daterange)
+    if model_name in available_models:
+        return available_models[model_name](market_info, model_name, candle_size, train_daterange)
     else:
         return None
